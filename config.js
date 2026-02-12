6fe7861726edccfcd0ecec244d57566724abc918
aqcroft <aqcroft@gmail.com>
2026-02-12 19:57:50 +0000

    Add photo references to venue config

--- config.js
+++ config.js
@@ -3,9 +3,9 @@
 // Edit the host values to update the host name displayed on the button.
 
 const VENUE_CONFIG = {
-  'Hinckley':          { host: 'Paul',   pin: '8415' },
-  'Loughborough':      { host: 'Séan',   pin: '6339' },
-  'Market Harborough': { host: 'Adrian', pin: '0800' },
-  'Leicester':         { host: 'Emma',   pin: '0616' },
-  'Lutterworth':       { host: 'Emma',   pin: '0616' }
+  'Hinckley':          { host: 'Paul',   pin: '8415', photo: 'Paul_Horne.jpeg' },
+  'Loughborough':      { host: 'Séan',   pin: '6339', photo: 'Sean_Smith.jpeg' },
+  'Market Harborough': { host: 'Adrian', pin: '0800', photo: 'Adrian_Croft.jpeg' },
+  'Leicester':         { host: 'Emma',   pin: '0616', photo: 'Emma_Smith.jpeg' },
+  'Lutterworth':       { host: 'Emma',   pin: '0616', photo: 'Emma_Smith.jpeg' }
 };
