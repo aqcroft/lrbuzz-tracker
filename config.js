a3ffb83990da6427e1c2d5455588ab1bba8aecbc
aqcroft <aqcroft@gmail.com>
2026-02-12 20:10:34 +0000

    Fix Adrian's photo extension to .jpg

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const VENUE_CONFIG = {
   'Hinckley':          { host: 'Paul',   pin: '8415', photo: 'Paul_Horne.jpeg' },
   'Loughborough':      { host: 'Séan',   pin: '6339', photo: 'Sean_Smith.jpeg' },
-  'Market Harborough': { host: 'Adrian', pin: '0800', photo: 'Adrian_Croft.jpeg' },
+  'Market Harborough': { host: 'Adrian', pin: '0800', photo: 'Adrian_Croft.jpg' },
   'Leicester':         { host: 'Emma',   pin: '0616', photo: 'Emma_Smith.jpeg' },
   'Lutterworth':       { host: 'Emma',   pin: '0616', photo: 'Emma_Smith.jpeg' }
 };
